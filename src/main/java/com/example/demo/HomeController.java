9c1f6170feb23d4579ebd9f72c3e1c5ca4b4ab28
fdmckie <fdmckie@gmail.com>
2019-08-20 09:41:51 -0400

    2.03 Handling Form Values -- The Easy Way exercise

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -1,4 +1,4 @@
-import com.example.demo.Employee;
+package com.example.demo;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
